b127f4430ea622dd163a3fe379f3f66cecdb5c5f
Reignard Uy <reignard.uy@dcmail.ca>
2023-02-11 16:00:49 -0500

    First commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# Webd6201-Lab1-Arslan-Reignard
---
+++ Scripts/EventHandler.js
@@ -0,0 +1,112 @@
+/**
+ * Name: Reignard Uy & Arslan Mahmood
+ * StudentID: 100757893 &
+ * Date: 02/06/23
+ */
+
+var userDisplay = document.getElementById("userDisplay");
+
+// Validating Contact forms
+function validateForm() {
+    let x = document.forms["myForm"]["username"].value;
+    let y = document.forms["myForm"]["email"].value;
+    let z = document.forms["myForm"]["message"].value;
+    if (x == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+    if (y == "") {
+        alert("Email must be given");
+        return false;
+    }
+    console.log(`Username: ${x} Email: ${y} Message: ${z}`)
+    userDisplay.textContent = `Username: ${x} Email: ${y} Message: ${z}`;
+    userDisplay.style.color = 'white';
+    userDisplay.style.fontSize = '20px';
+    userDisplay.style.fontFamily = 'Arial';
+    return false;
+}
+
+// JavaText
+function javaText(){
+    var txt = document.createElement("txt");
+    txt.id = "jstxt";
+    txt.innerHTML = "Using JavaScript To Write This Text."
+    document.body.appendChild(txt);
+}
+
+// Change the Product link to Projects
+function changeLink() {
+    var change = document.getElementById("change");
+    change.innerHTML = '<i class="fa-solid fa-sheet-plastic" style="font-size:20px;"></i>Projects';
+}
+
+// Welcome Text
+function welcomeHeader(){
+    var welcomeMsg = document.createElement("p");
+    welcomeMsg.id = "welcome";
+    welcomeMsg.innerHTML = "Welcome To Our Lab 1 Website";
+    document.body.appendChild(welcomeMsg);
+}
+// Products text 
+function updateProjects() {
+    var CalcPic = document.getElementById("CalcPic");
+    CalcPic.innerHTML = "Coded a small project that involved creating a workable Calculator";
+
+    var randomNumber = document.getElementById("randomNumber");
+    randomNumber.innerHTML = "Created a basic random number generator, that allowed user to ask how much numbers they want generated";
+
+    var smallGame = document.getElementById("smallGame");
+    smallGame.innerHTML = "Basic simple code that allows user to choose between rock, paper, and scissor to face off againts the AI";
+}
+
+// Services text 
+function updateWebDev() {
+    var webDev = document.getElementById("webDev");
+    webDev.innerHTML = "Confident in our skills of web development despite being fairly new in the field.";
+
+    var webDesign = document.getElementById("webDesign");
+    webDesign.innerHTML = "We strive for efficiency and the best quality when designing web pages.";
+
+    var ctmCode = document.getElementById("ctmCode");
+    ctmCode.innerHTML = "Continue to work on small projects to better our coding skills.";
+}
+
+// About us
+function aboutUs() {
+    var Rey = document.getElementById("Rey");
+    Rey.innerHTML = "Hi I'm Reignard, or Rey. I'm currently 21 years old, I enjoy web developement, and on my free time I read, and play basketball.";
+    
+    var Arslan = document.getElementById("Arslan");
+    Arslan.innerHTML = "lorem";
+}
+
+// Add nav link
+function addLink() {
+    let navbar = document.querySelector(".navbar-nav");
+    let aboutUs = document.querySelectorAll(".navbar-nav .nav-item")[3];
+    let humanResources = document.createElement("li");
+    humanResources.classList.add("nav-item");
+    let link = document.createElement("a");
+    link.classList.add("nav-link");
+    link.setAttribute("aria-current", "page");
+    link.setAttribute("href", "./humanresources.html");
+    link.innerHTML = '<i class="fa-solid fa-address-card"></i>Human Resources';
+    humanResources.appendChild(link);
+    navbar.insertBefore(humanResources, aboutUs.nextSibling);
+}
+
+// Copyright
+function Copyright(){
+    const currentYear = new Date().getFullYear();
+    document.getElementById("currentYear").innerHTML = currentYear;
+}
+
+// Redirection 
+document.getElementById("submitbutton").addEventListener("click", function () {
+    console.log("User Information:", document.getElementById("myForm").elements);
+    validateForm();
+    setTimeout(function () {
+        window.location.href = "./index.html";
+    }, 3000);
+});
